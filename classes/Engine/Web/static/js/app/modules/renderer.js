cdc1c7e6ba08a5e99cdb4384f321b7a4540c5e95
afwbkbc <afwbkbc@inbox.lv>
2019-01-01 02:03:50 +0200

    refactoring

--- classes/Engine/Web/static/js/app/modules/renderer.js
+++ classes/Engine/Web/static/js/app/modules/renderer.js
@@ -1,56 +1,54 @@
 window.App.Extend({
 	
-	canvases: {},
-	
-	renderer_shapes: {},
+	renderer: {
+		canvases: {},
+		shapes: {},
+	},
 	
 	StartRenderer: function() {
-		//this.body.html( '' ).append( this.viewport );
-		//this.ctx = this.viewport[0].getContext( '2d' );
 	},
 	
 	StopRenderer: function() {
-		//this.body.html( '' );
-		//this.viewport.html( '' );
-		//this.ctx = null;
-		for ( var k in this.canvases )
+		for ( var k in this.renderer.canvases )
 			this.RemoveCanvas( k );
 	},
 	
 	AddRendererShape: function( shape, obj ) {
-		this.renderer_shapes[ shape ] = obj;
+		this.renderer.shapes[ shape ] = obj;
 	},
 	
 	AddCanvas: function( id, width, height ) {
-		if ( this.canvases[ id ] ) {
+		if ( this.renderer.canvases[ id ] ) {
 			console.log( 'duplicate canvas id' );
 			return;
 		}
-		this.canvases[ id ] = $( '<canvas id="viewport' + id + '" width="' + width + '" height="' + height + '"></canvas>' );
-		this.canvases[ id ].appendTo( $( 'body' ) );
+		var canvas = $( '<canvas id="viewport' + id + '" width="' + width + '" height="' + height + '"></canvas>' ).appendTo( $( 'body' ) );
+		this.renderer.canvases[ id ] = {
+			canvas: canvas,
+			ctx: canvas[ 0 ].getContext( '2d' ),
+		};
 	},
 	
 	RemoveCanvas: function( id ) {
-		if ( !this.canvases[ id ] ) {
+		if ( !this.renderer.canvases[ id ] ) {
 			console.log( 'canvas not found' );
 			return;
 		}
-		this.canvases[ id ].remove();
-		delete this.canvases[ id ];
+		this.renderer.canvases[ id ].canvas.remove();
+		delete this.renderer.canvases[ id ];
 	},
 	
 	Draw: function( canvas, shape, parameters ) {
-		if ( !this.canvases[ canvas ] ) {
+		if ( !this.renderer.canvases[ canvas ] ) {
 			console.log( 'canvas not found' );
 			return;
 		}
-		var ctx = this.canvases[ canvas ][ 0 ].getContext( '2d' )
-		var obj = this.renderer_shapes[ shape ];
+		var obj = this.renderer.shapes[ shape ];
 		if ( !obj ) {
 			console.log( 'invalid shape "' + shape + '"' );
 			return;
 		}
-		obj.Draw( ctx, parameters );
+		obj.Draw( this.renderer.canvases[ canvas ].ctx, parameters );
 	},
 	
 });
